5eb4b03d1241987100fd08e33b43b9b2bbd51fdf
NateWebber <nwebber@u.rochester.edu>
2021-04-02 16:02:11 -0400

    initial nav_graph setup

--- app/src/main/java/com/nwebber/simonsays/ui/main/WelcomeFragment.kt
+++ app/src/main/java/com/nwebber/simonsays/ui/main/WelcomeFragment.kt
@@ -18,7 +18,7 @@ class WelcomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.title_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
