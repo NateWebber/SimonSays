e9d349a0c8e1b699eb6fbafaae5304ebe3ed825b
NateWebber <nwebber@u.rochester.edu>
2021-04-05 14:00:48 -0400

    recycler view now displays difficulty options

--- app/src/main/java/com/nwebber/simonsays/ui/main/ConfigFragment.kt
+++ app/src/main/java/com/nwebber/simonsays/ui/main/ConfigFragment.kt
@@ -3,11 +3,16 @@ package com.nwebber.simonsays.ui.main
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.inflate
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.observe
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nwebber.simonsays.R
 
 class ConfigFragment : Fragment() {
@@ -16,7 +21,10 @@ class ConfigFragment : Fragment() {
     }
     private val sharedViewModel: MainViewModel by activityViewModels()
 
+
     private lateinit var playButton: Button
+    private lateinit var selectedDifficulty: DifficultyDefinition
+    private lateinit var recycler: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +33,54 @@ class ConfigFragment : Fragment() {
         val view = inflater.inflate(R.layout.config_fragment, container, false)
 
         playButton = view.findViewById(R.id.play_button)
+        recycler = view.findViewById(R.id.recyclerView)
+        recycler.layoutManager = LinearLayoutManager(context)
 
         playButton.setOnClickListener {
             view.findNavController().navigate(R.id.action_configFragment_to_gameFragment)
         }
+        selectedDifficulty = sharedViewModel.difficultyMedium
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sharedViewModel.difficultyVocab.observe(viewLifecycleOwner, {
+            recycler.adapter = DifficultyAdapter(it)
+        })
+    }
+
+
+
+    private inner class DifficultyViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener{
+        private val nameTextView: TextView = itemView.findViewById(R.id.name_textView)
+        private lateinit var difficulty: DifficultyDefinition
+        init{
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            selectedDifficulty = difficulty
+        }
+
+        fun bind(difficulty: DifficultyDefinition){
+            this.difficulty = difficulty
+            nameTextView.text = difficulty.name
+        }
+    }
+
+    private inner class DifficultyAdapter(private val list: List<DifficultyDefinition>) : RecyclerView.Adapter<DifficultyViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DifficultyViewHolder {
+            val view = layoutInflater.inflate(R.layout.recycler_item, parent, false)
+            return DifficultyViewHolder(view)
+        }
+
+        override fun getItemCount() = list.size
+
+        override fun onBindViewHolder(holder: DifficultyViewHolder, position: Int) {
+            holder.bind(list[position])
+        }
+
     }
 }
--- app/src/main/java/com/nwebber/simonsays/ui/main/MainViewModel.kt
+++ app/src/main/java/com/nwebber/simonsays/ui/main/MainViewModel.kt
@@ -1,41 +1,70 @@
 package com.nwebber.simonsays.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.nwebber.simonsays.ui.main.model.GameModel
+
+class DifficultyVocab : ArrayList<DifficultyDefinition>()
+
+data class DifficultyDefinition(val name: String, val id: Int, val multiplier: Double) {}
 
 class MainViewModel : ViewModel() {
     var current_theme = 0
     var current_difficulty = 1
+
     //var current_turn = 0
     var current_score = 0
     var currentIndex = 0
     var gameModel = GameModel()
 
-    fun startGame(){
+    //difficulty data
+    val difficultyEasy = DifficultyDefinition("Easy", 0, 1.33)
+    val difficultyMedium = DifficultyDefinition("Medium", 1, 1.0)
+    val difficultyHard = DifficultyDefinition("Hard", 2, 0.66)
+
+    val difficultyList = listOf(difficultyEasy, difficultyMedium, difficultyHard)
+    private val _difficultyVocab = MutableLiveData<List<DifficultyDefinition>>()
+    var difficultyVocab: LiveData<List<DifficultyDefinition>> = _difficultyVocab
+
+    init{
+        _difficultyVocab.value = difficultyList
+        difficultyVocab = _difficultyVocab
+    }
+
+    fun startGame() {
         gameModel.newGame()
         currentIndex = 0
         current_score = 0
         simonTurn()
     }
-    fun checkInput(input: Int) : Boolean{
-        if(gameModel.checkInput(input, currentIndex)){
+
+    fun checkInput(input: Int): Boolean {
+        if (gameModel.checkInput(input, currentIndex)) {
             currentIndex++
             return true
         }
         return false
     }
-    fun simonTurn(){
+
+    fun simonTurn() {
         gameModel.addToPattern()
-        currentIndex=0
+        currentIndex = 0
     }
-    fun turnCompleted(): Boolean{
+
+    fun turnCompleted(): Boolean {
         return (currentIndex == gameModel.currentPattern.size)
     }
-    fun getPatternLength(): Int{
+
+    fun getPatternLength(): Int {
         return gameModel.currentPattern.size
     }
-    fun getPatternAtIndex(index: Int): Int{
+
+    fun getPatternAtIndex(index: Int): Int {
         return gameModel.currentPattern[index]
     }
+
+
+
 }
 
